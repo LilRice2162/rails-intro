1a68e8d1a908a891a421ade6121010f84a8d9492
Truong <Truongdavid2162@yahoo.com>
2024-08-19 15:59:29 +0200

    challenge 2 done

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,8 @@ class QuestionsController < ApplicationController
       @answer = 'Great!'
     elsif params[:question].include?('?')
       @answer = 'Silly question, get dressed and go to work!.'
+    else
+      @answer = "I don't care, get dressed and go to work!"
     end
   end
 end
